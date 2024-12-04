52157a05eaac1a6e18d01587b3fcdbaffe898a39
01Bhavesh <gharatbhavesh20@gmail.com>
2024-12-04 16:55:26 +0530

    Assignment

---
+++ AssignmentDotNet/AssignmentDotNet/Controllers/CategoryController.cs
@@ -0,0 +1,51 @@
+﻿using AssignmentDotNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssignmentDotNet.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ProductDb conn;
+        public CategoryController(ProductDb _product)
+        {
+            conn = _product;            
+        }
+
+        public IActionResult GetCategory()
+        {
+            List<Category> categories = conn.categories.ToList();
+            return View(categories);
+        }
+        [HttpGet]
+        public IActionResult PostCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult PostCategory(Category category)
+        {
+            conn.categories.Add(category);
+            conn.SaveChanges();
+            return RedirectToAction("GetCategory");
+        }
+        public IActionResult EditCategory(int id)
+        {
+            Category cat = conn.categories.Find(id);
+            return View(cat);
+        }
+        [HttpPost]
+        public IActionResult EditCategory(Category category)
+        {
+            conn.categories.Update(category);
+            conn.SaveChanges();
+            return RedirectToAction("GetCategory");
+        }
+        public IActionResult DeleteCategory(int id)
+        {
+            Category cat = conn.categories.Find(id);
+            conn.categories.Remove(cat);
+            conn.SaveChanges();
+            return RedirectToAction("GetCategory");
+        }
+    }
+}
---
+++ AssignmentDotNet/AssignmentDotNet/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using AssignmentDotNet.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace AssignmentDotNet.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductDb conn;
+
+        public ProductController(ProductDb _productdb)
+        {
+            conn = _productdb;
+        }
+
+        public IActionResult GetProduct()
+        {
+            var products = conn.products.Include(p => p.CategoryName).ToList();
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult PostProduct()
+        {
+            ViewBag.CategoryList = new SelectList(conn.categories, "CategoryId", "CategoryName");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult PostProduct(Product p)
+        {
+                conn.products.Add(p);
+                conn.SaveChanges();
+                return RedirectToAction("GetProduct");
+        }
+
+        [HttpGet]
+        public IActionResult EditProduct(int id)
+        {
+            var product = conn.products.Find(id);
+            ViewBag.CategoryList = new SelectList(conn.categories, "CategoryId", "CategoryName");
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult EditProduct(Product p)
+        {
+          
+                conn.products.Update(p);
+                conn.SaveChanges();
+                return RedirectToAction("GetProduct");
+            
+        }
+
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = conn.products.Find(id);
+            conn.products.Remove(product);
+            conn.SaveChanges();
+            return RedirectToAction("GetProduct");
+        }
+    }
+}
